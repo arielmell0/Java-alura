5f5e35fb50909d9a959cceb3610ca54b6f3ce3b1
Ariel <arielmello@rede.ulbra.br>
2021-01-13 04:55:55 -0300

    Aulas java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,15 @@
+
+public class TestaVariaveis {
+
+	public static void main (String[] args) {
+		System.out.println("Hello, new test");
+		
+		int age;
+		age = 37;
+		System.out.println(age);
+		
+		double age2;
+		age2 = 12.5;
+		System.out.println(age2);
+	}
+}
