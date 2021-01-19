ebb42550c6336da0b8f03c1a2539126e9889263f
Ariel <arielmello@rede.ulbra.br>
2021-01-19 12:47:27 -0300

    contador ++

--- sintaxe-variaveis-e-fluxo/src/TestaWhile.java
+++ sintaxe-variaveis-e-fluxo/src/TestaWhile.java
@@ -3,9 +3,11 @@ public class TestaWhile {
 	
 	public static void main (String[] args) {
 		int contador = 0;
-		while (contador < 10) {
+		while (contador <= 10) {
 			System.out.println(contador);
-			contador = contador +1;
+			//contador = contador +1; or
+			//contador += 1; or
+			contador++;
 		}
 	}
 	
